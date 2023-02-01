733fe2348494f565afb2a54c2ef3b341f721de8f
Victor Okonkwo <emmyvic98@gmail.com>
2023-02-01 16:05:12 +0000

    nft cards

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const ConnectButton = ({setConnectWalletModal}) => {
   return (
-    <button className="menubar-item btn btn-primary" onClick={() => setConnectWalletModal(true)}>Connect wallet</button>
+    <button className="menubar-item btn btn-primary-transparent" onClick={() => setConnectWalletModal(true)}>Connect wallet</button>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -66,14 +66,59 @@ export const Home = () => {
         </div>
         <div className="grid">
           <div className="grid-item">
-            <div className="card border-0">
+            <div className="card nft-card border-0">
               <img
                 src="https://i.seadn.io/gae/JSABuWLdFZdOW6pyaNXRfT8MBhVmw18xMoTAF9kRNc76-86fTg_rsVD5H9z3tsID1c8kaeieZNk7fqZU3Fntkdg7EL5EB67PtV6dog?fit=max&w=350&auto=format"
                 className="card-image-top nft-card-image"
                 alt="nft_image"
               />
-              <span className="badge mint_serial">#22747</span>
-              <div className="card-body"></div>
+              <div className="card-body py-1">
+                <div className="nft-info">
+                  <span className="badge mint_serial">#22747</span>
+                  <span className="badge time_left">5 hours left!</span>
+                </div>
+                <h5 className="nft_heading my-1">
+                  <span>Mutant Ape Yacht</span>
+                  <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
+                </h5>
+                <div className="nft-value my-1 mb-2">
+                  <small>Value:</small>
+                  <span className="amount">$42,427</span>
+                </div>
+                <Link to={"/"} className="btn btn-primary d-block mb-3">Enter Now</Link>
+                <div className="nft-card-footer text-center mb-2">
+                  <span className="text-uppercase fw-bold mb-1">Join 218 Superwallers</span>
+                  <span>Closes: 3 Feb 2013</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid-item">
+            <div className="card nft-card border-0">
+              <img
+                src="https://i.seadn.io/gae/JSABuWLdFZdOW6pyaNXRfT8MBhVmw18xMoTAF9kRNc76-86fTg_rsVD5H9z3tsID1c8kaeieZNk7fqZU3Fntkdg7EL5EB67PtV6dog?fit=max&w=350&auto=format"
+                className="card-image-top nft-card-image"
+                alt="nft_image"
+              />
+              <div className="card-body py-1">
+                <div className="nft-info">
+                  <span className="badge mint_serial">#22747</span>
+                  <span className="badge time_left">5 hours left!</span>
+                </div>
+                <h5 className="nft_heading my-1">
+                  <span>Mutant Ape Yacht</span>
+                  <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
+                </h5>
+                <div className="nft-value my-1 mb-2">
+                  <small>Value:</small>
+                  <span className="amount">$42,427</span>
+                </div>
+                <Link to={"/"} className="btn btn-primary d-block mb-3">Enter Now</Link>
+                <div className="nft-card-footer text-center mb-2">
+                  <span className="text-uppercase fw-bold mb-1">Join 218 Superwallers</span>
+                  <span>Closes: 3 Feb 2013</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
