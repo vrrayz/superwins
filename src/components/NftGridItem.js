119b6dfd9616b4550337fa015afb816957dbc2bd
Victor Okonkwo <emmyvic98@gmail.com>
2023-02-02 00:34:39 +0000

    grid item refactor

---
+++ src/components/NftGridItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const NftGridItem = ({id,imgSrc,mintNumber,timeLeft,name,value,totalParticipants,closeDate}) => {
+  return (
+    <div className="grid-item">
+      <div className="card nft-card border-0">
+        <img
+          src={imgSrc}
+          className="card-image-top nft-card-image"
+          alt="nft_image"
+        />
+        <div className="card-body py-1">
+          <div className="nft-info">
+            <span className="badge mint_serial">#{mintNumber}</span>
+            <span className="badge time_left">{timeLeft}</span>
+          </div>
+          <h5 className="nft_heading my-1">
+            <span>{name}</span>
+            <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
+          </h5>
+          <div className="nft-value my-1 mb-2">
+            <small>Value:</small>
+            <span className="amount">${value}</span>
+          </div>
+          <Link to={"/"} className="btn btn-primary d-block mb-3">
+            Enter Now
+          </Link>
+          <div className="nft-card-footer text-center mb-2">
+            <span className="text-uppercase fw-bold mb-1">
+              Join {totalParticipants} Superwallers
+            </span>
+            <span>Closes: {closeDate}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/data/nftCompetitionData.js
@@ -0,0 +1,24 @@
+export const nftCompetitionData = [
+  {
+    id: 1,
+    imgSrc:
+      "https://i.seadn.io/gae/JSABuWLdFZdOW6pyaNXRfT8MBhVmw18xMoTAF9kRNc76-86fTg_rsVD5H9z3tsID1c8kaeieZNk7fqZU3Fntkdg7EL5EB67PtV6dog?fit=max&w=350&auto=format",
+    mintNumber: 22747,
+    timeLeft: "5 hours left!",
+    name: "Mutant Ape Yacht",
+    value: 42427,
+    totalParticipants: 218,
+    closeDate: "3 Feb 2013",
+  },
+  {
+    id: 2,
+    imgSrc:
+      "https://i.seadn.io/gae/JSABuWLdFZdOW6pyaNXRfT8MBhVmw18xMoTAF9kRNc76-86fTg_rsVD5H9z3tsID1c8kaeieZNk7fqZU3Fntkdg7EL5EB67PtV6dog?fit=max&w=350&auto=format",
+    mintNumber: 227,
+    timeLeft: "5 hours left!",
+    name: "Mutant Ape Yacht",
+    value: 4027,
+    totalParticipants: 100,
+    closeDate: "3 Jan 2020",
+  },
+];
