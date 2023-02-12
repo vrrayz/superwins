a449c69b7336a16a4fa8024bf31c1a489d8d1be9
Victor Okonkwo <emmyvic98@gmail.com>
2023-02-12 16:19:26 +0000

    refactored styled components

--- src/components/NftGrid/NftGrid.js
+++ src/components/NftGrid/NftGrid.js
@@ -10,9 +10,17 @@ export const NftGrid = ({ nftCompetitionData, isCarouselView }) => {
           return `${templateColumn}%`;
         })
       : `${templateColumn}% `.repeat(parseInt(100 / templateColumn));
-  const CustomNftGrid = styled.div`
+  return (
+    <CustomNftGrid templateColumn={gridTemplateColumnCondition} className="nft-grid pb-3">
+      {nftCompetitionData.map((competition) => {
+        return <NftGridItem key={competition.id} {...competition} />;
+      })}
+    </CustomNftGrid>
+  );
+};
+const CustomNftGrid = styled.div`
     display: grid;
-    grid-template-columns: ${gridTemplateColumnCondition(49)};
+    grid-template-columns: ${props => props.templateColumn(49)};
     overflow-x: scroll;
     &::-webkit-scrollbar {
       height: 4px;
@@ -22,20 +30,12 @@ export const NftGrid = ({ nftCompetitionData, isCarouselView }) => {
       border-radius: 25px;
     }
     @media (min-width: 768px) {
-      grid-template-columns: ${gridTemplateColumnCondition(32)};
+      grid-template-columns: ${props => props.templateColumn(32)};
       column-gap: 8px;
       margin-left: 8px;
     }
 
     @media (min-width: 1024px) {
-      grid-template-columns: ${gridTemplateColumnCondition(19.5)};
+      grid-template-columns: ${props => props.templateColumn(19.5)};
     }
   `;
-  return (
-    <CustomNftGrid className="nft-grid pb-3">
-      {nftCompetitionData.map((competition) => {
-        return <NftGridItem key={competition.id} {...competition} />;
-      })}
-    </CustomNftGrid>
-  );
-};
