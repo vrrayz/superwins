1816ddb08b21844ed8dcb9e7f2a2d86390252ba5
Victor Okonkwo <emmyvic98@gmail.com>
2023-02-14 23:24:20 +0000

    refactored Chat component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 import { tabData } from "../../data/tabData";
 import { nftCompetitionData } from "../../data/nftCompetitionData";
-import avatarImg from "../../assets/img/avatars/default.png";
 
 import { Tabs } from "../Tabs/Tabs";
 import { LiveSection } from "./LiveSection";
 import { CompletedSection } from "./CompletedSection";
 import { ActivitySection } from "./ActivitySection";
 import styled from "styled-components";
+import { LiveChat } from "./LiveChat";
 
 export const Home = () => {
   const [urlParams, setUrlParams] = useState(new URLSearchParams(""));
@@ -53,134 +53,13 @@ export const Home = () => {
         )}
         {tabs.activity && <ActivitySection />}
       </div>
-      <LiveChatContainer>
-        <ChatsSection>
-          <ChatInfo>
-            <img src={avatarImg} alt="user" className="mt-1" />
-            <div>
-              <User className="mb-1">noobsage</User>
-              <Message>This is my message to everyone on this platform</Message>
-            </div>
-          </ChatInfo>
-          <ChatInfo>
-            <img src={avatarImg} alt="user" className="mt-1" />
-            <div>
-              <User className="mb-1">noobsage</User>
-              <Message>This is my message to everyone on this platform</Message>
-            </div>
-          </ChatInfo>
-          <ChatInfo>
-            <img src={avatarImg} alt="user" className="mt-1" />
-            <div>
-              <User className="mb-1">noobsage</User>
-              <Message>This is my message to everyone on this platform</Message>
-            </div>
-          </ChatInfo>
-          <ChatInfo>
-            <img src={avatarImg} alt="user" className="mt-1" />
-            <div>
-              <User className="mb-1">noobsage</User>
-              <Message>This is my message to everyone on this platform</Message>
-            </div>
-          </ChatInfo>
-          <ChatInfo>
-            <img src={avatarImg} alt="user" className="mt-1" />
-            <div>
-              <User className="mb-1">noobsage</User>
-              <Message>This is my message to everyone on this platform</Message>
-            </div>
-          </ChatInfo>
-        </ChatsSection>
-        <InputSection>
-          <ChatInput placeholder="Send a message" />
-          <SendMessageButton>
-            <i className="fa-regular fa-paper-plane"></i>
-          </SendMessageButton>
-        </InputSection>
-        <PlayersCountSection><i className="fa-solid fa-circle text-success"></i> 108 players online</PlayersCountSection>
-      </LiveChatContainer>
+      <LiveChat/>
     </HomeContainer>
   );
 };
-
 const HomeContainer = styled.div`
   @media (min-width: 1200px) {
     display: grid;
     grid-template-columns: 1fr 320px;
   }
 `;
-const LiveChatContainer = styled.aside`
-  display: none;
-  @media (min-width: 1200px) {
-    display: grid;
-    grid-template-rows: 1fr 50px 40px;
-    row-gap: 12px;
-    background-color: var(--color-dark-3);
-    position: fixed;
-    right: 0;
-    top: 0;
-    width: 320px;
-    height: 100vh;
-  }
-`;
-const ChatsSection = styled.section`
-  padding: 0px 16px;
-  display: flex;
-  flex-direction: column;
-  justify-content: end;
-`;
-const InputSection = styled.section`
-  padding: 0px 16px;
-  display: flex;
-  justify-content: center;
-`;
-const ChatInfo = styled.div`
-  display: flex;
-  column-gap: 8px;
-  img {
-    width: 28px;
-    height: 28px;
-    border-radius: 50%;
-  }
-`;
-const Message = styled.p`
-  font-size: 0.85rem;
-  line-height: 1.2;
-  color: var(--color-light-1);
-`;
-const User = styled.h5`
-  font-size: 1rem;
-  font-weight: bold;
-  color: var(--color-primary-red);
-`;
-const ChatInput = styled.input`
-  margin-right: 0px;
-  width: 100%;
-  border-top-left-radius: 25px;
-  border-bottom-left-radius: 25px;
-  border: none;
-  background-color: var(--color-grey-1);
-  color: var(--color-light-1);
-  padding: 4px;
-  padding-left: 16px;
-  font-size: 14px;
-
-  &:focus-visible {
-    border: none;
-    outline: none;
-  }
-`;
-const SendMessageButton = styled.button`
-  border-top-right-radius: 25px;
-  border-bottom-right-radius: 25px;
-  border: none;
-  background-color: var(--color-grey-1);
-  padding-right: 16px;
-  color: var(--color-light-1);
-`;
-const PlayersCountSection = styled.p`
-color: var(--color-light-1);
-font-size: 14px;
-margin: 0px 16px;
-// margin-top: 4px;
-`
---
+++ src/components/Home/LiveChat.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import styled from 'styled-components';
+import avatarImg from "../../assets/img/avatars/default.png";
+
+export const LiveChat = () => {
+  return (
+    <LiveChatContainer>
+        <ChatsSection>
+          <ChatInfo>
+            <img src={avatarImg} alt="user" className="mt-1" />
+            <div>
+              <User className="mb-1">noobsage</User>
+              <Message>This is my message to everyone on this platform</Message>
+            </div>
+          </ChatInfo>
+          <ChatInfo>
+            <img src={avatarImg} alt="user" className="mt-1" />
+            <div>
+              <User className="mb-1">noobsage</User>
+              <Message>This is my message to everyone on this platform</Message>
+            </div>
+          </ChatInfo>
+          <ChatInfo>
+            <img src={avatarImg} alt="user" className="mt-1" />
+            <div>
+              <User className="mb-1">noobsage</User>
+              <Message>This is my message to everyone on this platform</Message>
+            </div>
+          </ChatInfo>
+          <ChatInfo>
+            <img src={avatarImg} alt="user" className="mt-1" />
+            <div>
+              <User className="mb-1">noobsage</User>
+              <Message>This is my message to everyone on this platform</Message>
+            </div>
+          </ChatInfo>
+          <ChatInfo>
+            <img src={avatarImg} alt="user" className="mt-1" />
+            <div>
+              <User className="mb-1">noobsage</User>
+              <Message>This is my message to everyone on this platform</Message>
+            </div>
+          </ChatInfo>
+        </ChatsSection>
+        <InputSection>
+          <ChatInput placeholder="Send a message" />
+          <SendMessageButton>
+            <i className="fa-regular fa-paper-plane"></i>
+          </SendMessageButton>
+        </InputSection>
+        <PlayersCountSection><i className="fa-solid fa-circle text-success"></i> 108 players online</PlayersCountSection>
+      </LiveChatContainer>
+  )
+}
+const LiveChatContainer = styled.aside`
+  display: none;
+  @media (min-width: 1200px) {
+    display: grid;
+    grid-template-rows: 1fr 50px 40px;
+    row-gap: 12px;
+    background-color: var(--color-dark-3);
+    position: fixed;
+    right: 0;
+    top: 0;
+    width: 320px;
+    height: 100vh;
+  }
+`;
+const ChatsSection = styled.section`
+  padding: 0px 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+`;
+const InputSection = styled.section`
+  padding: 0px 16px;
+  display: flex;
+  justify-content: center;
+`;
+const ChatInfo = styled.div`
+  display: flex;
+  column-gap: 8px;
+  img {
+    width: 28px;
+    height: 28px;
+    border-radius: 50%;
+  }
+`;
+const Message = styled.p`
+  font-size: 0.85rem;
+  line-height: 1.2;
+  color: var(--color-light-1);
+`;
+const User = styled.h5`
+  font-size: 1rem;
+  font-weight: bold;
+  color: var(--color-primary-red);
+`;
+const ChatInput = styled.input`
+  margin-right: 0px;
+  width: 100%;
+  border-top-left-radius: 25px;
+  border-bottom-left-radius: 25px;
+  border: none;
+  background-color: var(--color-grey-1);
+  color: var(--color-light-1);
+  padding: 4px;
+  padding-left: 16px;
+  font-size: 14px;
+
+  &:focus-visible {
+    border: none;
+    outline: none;
+  }
+`;
+const SendMessageButton = styled.button`
+  border-top-right-radius: 25px;
+  border-bottom-right-radius: 25px;
+  border: none;
+  background-color: var(--color-grey-1);
+  padding-right: 16px;
+  color: var(--color-light-1);
+`;
+const PlayersCountSection = styled.p`
+color: var(--color-light-1);
+font-size: 14px;
+margin: 0px 16px;
+// margin-top: 4px;
+`
