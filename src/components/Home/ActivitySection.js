df6fbc4bab5222258b823ad94ae6d5d892b07b81
Victor Okonkwo <emmyvic98@gmail.com>
2023-02-15 14:05:09 +0000

    modified activity tab display for larger screens

--- src/components/Home/ActivitySection.js
+++ src/components/Home/ActivitySection.js
@@ -9,10 +9,13 @@ export const ActivitySection = () => {
   return (
     <>
       <ListGroupItem className="list-header">
-        <span>Competition</span> <span>Username</span>
+        <span>Competition</span>
+        <span>Username</span>
+        <span>Entries Purchased</span>
+        <span>Time</span>
       </ListGroupItem>
       <ListGroupItem>
-        <div>
+        <ListData>
           <CompetitionRewardSection>
             <img src={rewardImg} alt="reward" />
             <span>
@@ -23,85 +26,28 @@ export const ActivitySection = () => {
           <CompetitionEnteries>
             Entries <span className="entries">20</span>
           </CompetitionEnteries>
-        </div>
-        <div>
+        </ListData>
+        <ListData>
           <UserAvatarSection>
             <img src={avatarImg} alt="avatar" />
             <span>SuperRyan</span>
           </UserAvatarSection>
           <div className="text-center">
-          <Link to={"/"} className="timestamp">30 minutes ago <i className="fa-solid fa-arrow-up-right-from-square"></i></Link>
+            <Link to={"/"} className="timestamp">
+              30 minutes ago{" "}
+              <i className="fa-solid fa-arrow-up-right-from-square"></i>
+            </Link>
           </div>
-        </div>
-      </ListGroupItem>
-      <ListGroupItem>
-        <div>
-          <CompetitionRewardSection>
-            <img src={rewardImg} alt="reward" />
-            <span>
-              1 Eth
-              <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
-            </span>
-          </CompetitionRewardSection>
-          <CompetitionEnteries>
-            Entries <span className="entries">20</span>
-          </CompetitionEnteries>
-        </div>
-        <div>
-          <UserAvatarSection>
-            <img src={avatarImg} alt="avatar" />
-            <span>SuperRyan</span>
-          </UserAvatarSection>
-          <div className="text-center">
-          <Link to={"/"} className="timestamp">30 minutes ago <i className="fa-solid fa-arrow-up-right-from-square"></i></Link>
-          </div>
-        </div>
-      </ListGroupItem>
-      <ListGroupItem>
-        <div>
-          <CompetitionRewardSection>
-            <img src={rewardImg} alt="reward" />
-            <span>
-              1 Eth
-              <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
-            </span>
-          </CompetitionRewardSection>
-          <CompetitionEnteries>
-            Entries <span className="entries">20</span>
-          </CompetitionEnteries>
-        </div>
-        <div>
-          <UserAvatarSection>
-            <img src={avatarImg} alt="avatar" />
-            <span>SuperRyan</span>
-          </UserAvatarSection>
-          <div className="text-center">
-          <Link to={"/"} className="timestamp">30 minutes ago <i className="fa-solid fa-arrow-up-right-from-square"></i></Link>
-          </div>
-        </div>
-      </ListGroupItem>
-      <ListGroupItem>
-        <div>
-          <CompetitionRewardSection>
-            <img src={rewardImg} alt="reward" />
-            <span>
-              1 Eth
-              <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
-            </span>
-          </CompetitionRewardSection>
-          <CompetitionEnteries>
-            Entries <span className="entries">20</span>
-          </CompetitionEnteries>
-        </div>
-        <div>
-          <UserAvatarSection>
-            <img src={avatarImg} alt="avatar" />
-            <span>SuperRyan</span>
-          </UserAvatarSection>
-          <div className="text-center">
-          <Link to={"/"} className="timestamp">30 minutes ago <i className="fa-solid fa-arrow-up-right-from-square"></i></Link>
-          </div>
-        </div>
+        </ListData>
+        <ListData className="my-auto">
+          <span className="entries">20</span>
+        </ListData>
+        <ListData className="my-auto">
+          <Link to={"/"} className="timestamp">
+            30 minutes ago{" "}
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>
+          </Link>
+        </ListData>
       </ListGroupItem>
     </>
   );
@@ -115,10 +61,24 @@ const ListGroupItem = styled.div`
   grid-template-columns: 50% 50%;
   border-bottom: 1px solid rgba(255, 255, 255, 0.3);
 
+  span:nth-child(n + 3),
+  div:nth-child(nt + 3) {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    text-align: center;
+
+    span:nth-child(n + 2) {
+      display: block;
+    }
+  }
+
   &.list-header {
     font-weight: bold;
   }
-  .timestamp{
+  .timestamp {
     font-size: 12px;
   }
 `;
@@ -154,5 +114,13 @@ const UserAvatarSection = styled.div`
   }
   span {
     margin: auto 0px;
+  }
+`;
+const ListData = styled.div`
+  @media (min-width: 768px) {
+    & > span:nth-child(2),
+    & > div:nth-child(2) {
+      display: none;
+    }
   }
 `;
