ceb7d68cca172c416b7562a6f3b98ced24a7547f
Victor Okonkwo <emmyvic98@gmail.com>
2023-02-15 14:22:26 +0000

    refactored InnerTab

--- src/components/Home/CompletedSection.js
+++ src/components/Home/CompletedSection.js
@@ -1,56 +1,47 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { GridSection } from "../NftGrid/GridSection";
+import { InnerTab } from "./InnerTab";
 
-export const CompletedSection = ({nftCompetitionData}) => {
-    const [isHourlySchedule, setIsHourlySchedule] = useState(true);
-  
-    return (
-      <>
-        <InnerTabContainer>
-          <InnerTabItem
-            className={isHourlySchedule ? "active" : ""}
-            onClick={() => setIsHourlySchedule(true)}
-          >
-            Hourly Schedule
-          </InnerTabItem>
-          <InnerTabItem
-            className={isHourlySchedule ? "" : "active"}
-            onClick={() => setIsHourlySchedule(false)}
-          >
-            Head 2 Head
-          </InnerTabItem>
-        </InnerTabContainer>
-        {isHourlySchedule ? (
-          <GridSection
-            nftCompetitionData={nftCompetitionData}
-            className=""
-            sectionHeading={""}
-          />
-        ) : (
-          <GridSection
-            nftCompetitionData={nftCompetitionData}
-            className=""
-            sectionHeading={""}
-          />
-        )}
-      </>
-    );
-  };
-  const InnerTabContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    column-gap: 16px;
-    margin-top: 32px;
-    margin-bottom: 16px;
-  `;
-  const InnerTabItem = styled.div`
-    text-transform: uppercase;
-    color: var(--color-light-1);
-    font-size: 14px;
-  
-    &.active {
-      color: var(--color-primary-red);
-    }
-  `;
-  
+export const CompletedSection = ({ nftCompetitionData }) => {
+  const [isHourlySchedule, setIsHourlySchedule] = useState(true);
+
+  return (
+    <>
+      <InnerTab
+        isFirstItem={isHourlySchedule}
+        setIsFirstItem={setIsHourlySchedule}
+        items={["Hourly schedule", "Head 2 Head"]}
+      />
+      {isHourlySchedule ? (
+        <GridSection
+          nftCompetitionData={nftCompetitionData}
+          className=""
+          sectionHeading={""}
+        />
+      ) : (
+        <GridSection
+          nftCompetitionData={nftCompetitionData}
+          className=""
+          sectionHeading={""}
+        />
+      )}
+    </>
+  );
+};
+const InnerTabContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  column-gap: 16px;
+  margin-top: 32px;
+  margin-bottom: 16px;
+`;
+const InnerTabItem = styled.div`
+  text-transform: uppercase;
+  color: var(--color-light-1);
+  font-size: 14px;
+
+  &.active {
+    color: var(--color-primary-red);
+  }
+`;
---
+++ src/components/Home/InnerTab.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export const InnerTab = ({isFirstItem, setIsFirstItem,items}) => {
+    const [firstItem, secondItem] = items
+  return (
+    <InnerTabContainer>
+          <InnerTabItem
+            className={isFirstItem ? "active" : ""}
+            onClick={() => setIsFirstItem(true)}
+          >
+           {firstItem}
+          </InnerTabItem>
+          <InnerTabItem
+            className={isFirstItem ? "" : "active"}
+            onClick={() => setIsFirstItem(false)}
+          >
+            {secondItem}
+          </InnerTabItem>
+        </InnerTabContainer>
+  )
+}
+const InnerTabContainer = styled.div`
+display: flex;
+justify-content: center;
+column-gap: 16px;
+margin-top: 32px;
+margin-bottom: 16px;
+`;
+const InnerTabItem = styled.div`
+text-transform: uppercase;
+color: var(--color-light-1);
+font-size: 14px;
+
+&.active {
+  color: var(--color-primary-red);
+}
+`;
+
