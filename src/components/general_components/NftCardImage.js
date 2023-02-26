6c06f21fb72a241034aeff244a5cf199f4da3f20
Victor Okonkwo <emmyvic98@gmail.com>
2023-02-26 23:02:52 +0000

    refactored

---
+++ src/components/general_components/NftCardImage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NftCardImage = styled.img`
+  object-fit: cover;
+  height: 175px;
+  border-radius: 6px;
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+  cursor: pointer;
+`;
