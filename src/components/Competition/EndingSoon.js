6ba97f2b025b9e7ee8acfad5c7d45fa83f30bde6
Victor Okonkwo <emmyvic98@gmail.com>
2023-03-03 23:49:48 +0000

    ending soon grid modification

--- src/components/Competition/EndingSoon.js
+++ src/components/Competition/EndingSoon.js
@@ -10,19 +10,24 @@ export const EndingSoon = () => {
     <SectionWrapper id="endingSoon">
       <CustomCard className="py-3 px-1">
         <CardHead>Ending Soon</CardHead>
-        <EntryGrid>
+        <CustomGrid>
           <CompetitionCard>
             <CardImage
               alt="cardInfo"
               src="https://i.seadn.io/gae/ZxISqq2miZ2sg6J3b7Pq5pBGUjKrkFNgBIC6jXDFnEoi9S7D72m6F37dP7WoCEWDVAsAdtIH2HUmSsaKFetySd9_0bUlxy4BrB7rtIM?fit=max&w=350&auto=format"
             />
             <CardBody>
-              <CompetitionHeading>a KID called BEAST <i className="fa-solid fa-circle-check ms-1 text-primary"></i></CompetitionHeading>
+              <CompetitionHeading>
+                a KID called BEAST{" "}
+                <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
+              </CompetitionHeading>
               <Value>$3,247</Value>
               <CustomButton className="btn-primary d-block mx-auto mb-3 fw-bold w-100">
-                  Enter now
-                </CustomButton>
-              <SubText>Join <span>203</span> superwallers</SubText>
+                Enter now
+              </CustomButton>
+              <SubText>
+                Join <span>203</span> superwallers
+              </SubText>
             </CardBody>
           </CompetitionCard>
           <CompetitionCard>
@@ -31,15 +36,39 @@ export const EndingSoon = () => {
               src="https://i.seadn.io/gae/ZxISqq2miZ2sg6J3b7Pq5pBGUjKrkFNgBIC6jXDFnEoi9S7D72m6F37dP7WoCEWDVAsAdtIH2HUmSsaKFetySd9_0bUlxy4BrB7rtIM?fit=max&w=350&auto=format"
             />
             <CardBody>
-              <CompetitionHeading>a KID called BEAST <i className="fa-solid fa-circle-check ms-1 text-primary"></i></CompetitionHeading>
+              <CompetitionHeading>
+                a KID called BEAST{" "}
+                <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
+              </CompetitionHeading>
               <Value>$3,247</Value>
               <CustomButton className="btn-primary d-block mx-auto mb-3 fw-bold w-100">
-                  Enter now
-                </CustomButton>
-              <SubText>Join <span>203</span> superwallers</SubText>
+                Enter now
+              </CustomButton>
+              <SubText>
+                Join <span>203</span> superwallers
+              </SubText>
             </CardBody>
           </CompetitionCard>
-        </EntryGrid>
+          <CompetitionCard>
+            <CardImage
+              alt="cardInfo"
+              src="https://i.seadn.io/gae/ZxISqq2miZ2sg6J3b7Pq5pBGUjKrkFNgBIC6jXDFnEoi9S7D72m6F37dP7WoCEWDVAsAdtIH2HUmSsaKFetySd9_0bUlxy4BrB7rtIM?fit=max&w=350&auto=format"
+            />
+            <CardBody>
+              <CompetitionHeading>
+                a KID called BEAST{" "}
+                <i className="fa-solid fa-circle-check ms-1 text-primary"></i>
+              </CompetitionHeading>
+              <Value>$3,247</Value>
+              <CustomButton className="btn-primary d-block mx-auto mb-3 fw-bold w-100">
+                Enter now
+              </CustomButton>
+              <SubText>
+                Join <span>203</span> superwallers
+              </SubText>
+            </CardBody>
+          </CompetitionCard>
+        </CustomGrid>
       </CustomCard>
     </SectionWrapper>
   );
@@ -58,6 +87,14 @@ const CompetitionCard = styled.div`
   text-align: center;
   border-radius: 12px;
   margin: 0px 4px;
+  &:nth-child(3) {
+    display: none;
+  }
+  @media (min-width: 1024px) {
+    &:nth-child(3) {
+      display: block;
+    }
+  }
 `;
 const CardImage = styled.img`
   object-fit: cover;
@@ -70,19 +107,29 @@ const CardImage = styled.img`
   // margin-bottom: 16px;
 `;
 const CardBody = styled.div`
-padding: 8px;
-`
+  padding: 8px;
+`;
 const CompetitionHeading = styled.h6`
-font-weight: bold;
-font-size: .8rem;
-margin-bottom: 4px;
-`
+  font-weight: bold;
+  font-size: 0.8rem;
+  margin-bottom: 4px;
+`;
 const Value = styled.h5`
-font-weight: bolder;
-`
+  font-weight: bolder;
+`;
 const SubText = styled.span`
-font-size: .75rem;
-`
+  font-size: 0.75rem;
+`;
 const CustomButton = styled(Button)`
-margin-bottom: 0px !important;
-`
+  margin-bottom: 0px !important;
+`;
+const CustomGrid = styled.div`
+  display: grid;
+  padding: 0 0.4rem;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  column-gap: 0.7rem;
+  row-gap: 0.7rem;
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+`;
