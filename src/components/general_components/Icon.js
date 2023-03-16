0b9b79bd3c9ce19e49ba61f0b0ef118c8014bcd6
Victor Okonkwo <emmyvic98@gmail.com>
2023-03-16 06:33:28 +0000

    Image Icon component

--- src/components/general_components/Icon.js
+++ src/components/general_components/Icon.js
@@ -4,3 +4,7 @@ export const Icon = styled.i`
   color: var(--color-blue-1);
   font-size: 18px;
 `;
+export const ImgIcon = styled.img`
+width: ${props => props.width || '24'}px;
+height: ${props => props.height || '24'}px;
+`
