d87c250387a717564c2c4b73253b2c0b95bb80a6
Victor Okonkwo <emmyvic98@gmail.com>
2023-03-17 18:51:13 +0000

    signup component

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { TextLarge, TextInlineSmall } from '../general_components/typography'
+import { Checkbox, Input } from '../general_components/Input'
+import { Button } from '../general_components/Button'
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
+    <>
+    <TextLarge className='text-uppercase'>Welcome</TextLarge>
+    <Input placeholder="Username" className='mb-3' />
+    <Input placeholder="Email" className='mb-3' />
+    <Button className='btn-primary d-block w-100 mb-3' disabled>Complete Registration</Button>
+    <Checkbox /> <TextInlineSmall style={{fontSize: '14px'}} className='text__grey-2'>By creating your account you confirm that you are at least 18 years old</TextInlineSmall>
+    </>
   )
 }
