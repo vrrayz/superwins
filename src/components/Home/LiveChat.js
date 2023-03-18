e25300617dadcc6921ec8a83ecb3deaa0c2e3c9a
Victor Okonkwo <emmyvic98@gmail.com>
2023-03-18 16:03:16 +0000

    refactored

--- src/components/Home/LiveChat.js
+++ src/components/Home/LiveChat.js
@@ -1,5 +1,3 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import avatarImg from "../../assets/img/avatars/default.png";
 
