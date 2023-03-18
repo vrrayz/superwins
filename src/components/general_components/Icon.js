9896407534888be2e666a947a2c87c94668a10f5
Victor Okonkwo <emmyvic98@gmail.com>
2023-03-18 17:11:24 +0000

    refactored icon

--- src/components/general_components/Icon.js
+++ src/components/general_components/Icon.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Icon = styled.i`
   color: var(--color-blue-1);
-  font-size: 18px;
+  font-size: ${props => props.font || '18'}px;
 `;
 export const ImgIcon = styled.img`
 width: ${props => props.width || '24'}px;
