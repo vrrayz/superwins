f527813d6d91b91bb1cba1e9e7a60483c9c34738
Victor Okonkwo <emmyvic98@gmail.com>
2023-03-18 16:07:48 +0000

    refactored custom section wrapper

--- src/components/Auth/AuthRoutes.js
+++ src/components/Auth/AuthRoutes.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import { CustomCard, CardBody } from "../general_components/CustomCard";
-import { SectionWrapper } from "../general_components/SectionWrapper";
+import { CenteredSectionWrapper } from "../general_components/SectionWrapper";
 import { Signup } from "./Signup";
 import { Verification } from "./Verification";
 
 export const AuthRoutes = ({ logo }) => {
   return (
-    <CustomSectionWrapper>
+    <CenteredSectionWrapper>
       <Logo src={logo} />
       <CustomCard className="mt-4 p-4">
         <CardBody>
@@ -18,15 +18,9 @@ export const AuthRoutes = ({ logo }) => {
           </Routes>
         </CardBody>
       </CustomCard>
-    </CustomSectionWrapper>
+    </CenteredSectionWrapper>
   );
 };
-const CustomSectionWrapper = styled(SectionWrapper)`
-  display: grid;
-  grid-template-rows: 150px 2fr;
-  max-width: 400px;
-  margin:auto;
-`;
 const Logo = styled.img`
   margin: auto;
   margin-top: 48px;
--- src/components/general_components/SectionWrapper.js
+++ src/components/general_components/SectionWrapper.js
@@ -11,3 +11,9 @@ export const SectionWrapper = ({children, className}) => {
 const Wrapper = styled.div`
   padding: 0px 16px;
 `;
+export const CenteredSectionWrapper = styled(SectionWrapper)`
+  display: grid;
+  grid-template-rows: 150px 2fr;
+  max-width: 400px;
+  margin:auto;
+`;
