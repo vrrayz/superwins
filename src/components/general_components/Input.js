586f7565199797a21763a85d26ba7fae6a563142
Victor Okonkwo <emmyvic98@gmail.com>
2023-03-18 17:53:39 +0000

    added styling for append button

--- src/components/general_components/Input.js
+++ src/components/general_components/Input.js
@@ -5,7 +5,7 @@ margin-right: 0px;
 width: 100%;
 border-radius:6px;
 border: none;
-background-color: var(--color-grey-1);
+background-color: var(--color-primary-darkgrey-1);
 color: var(--color-light-1);
 padding: 12px 4px;
 padding-left: 16px;
@@ -14,6 +14,24 @@ font-size: 14px;
 &:focus-visible {
   border: none;
   outline: none;
+}
+`
+export const AppendButton = styled.button`
+border-top-right-radius: 6px;
+border-bottom-right-radius: 6px;
+border: none;
+background-color: var(--color-primary-darkgrey-1);
+padding-right: 16px;
+color: var(--color-light-1);
+`;
+export const InputGroupAppend = styled.div`
+display: flex;
+justify-content: center;
+
+${Input} {
+  border-radius: 0px;
+  border-top-left-radius: 6px;
+  border-bottom-left-radius: 6px;
 }
 `
 export const Checkbox = styled.input.attrs({type: 'checkbox'})`
